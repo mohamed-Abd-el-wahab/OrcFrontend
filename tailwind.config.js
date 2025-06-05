94e5373b948717633d4fe80aef934f67c80f5bff
mohame Abd Elwahab <93092315+mohamed-Abd-el-wahab@users.noreply.github.com>
2025-06-05 05:49:18 +0300

    Install and configure framer-motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'brand': {
-          blue: '#0D0DFF',
-          black: '#000000',
-          'neutral-20': '#333333',
-          'neutral-5': '#F2F2F2',
-        }
-      },
-      fontFamily: {
-        sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
-      },
-      maxWidth: {
-        'container': '1280px',
-      },
-      spacing: {
-        '18': '4.5rem',
-        '22': '5.5rem',
-      },
-      screens: {
-        'xs': '480px',
-        'sm': '640px',
-        'md': '960px',
-        'lg': '1280px',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
