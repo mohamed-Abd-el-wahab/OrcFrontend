fbe1ae1a4c3e8c26bfedd21873578b5f698b9942
mohame Abd Elwahab <93092315+mohamed-Abd-el-wahab@users.noreply.github.com>
2025-06-07 05:39:50 +0300

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
-import ServicesSection from '../components/home/ServicesSection';
 
 const Home = () => {
   return (
     <div className="bg-black">
       <Hero />
-      <ServicesSection />
     </div>
   );
 };
