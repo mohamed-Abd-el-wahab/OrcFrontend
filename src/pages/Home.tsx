e0fb2215e5e695c76f86d8845415c46268a0c815
mohame Abd Elwahab <93092315+mohamed-Abd-el-wahab@users.noreply.github.com>
2025-06-07 05:29:44 +0300

    Redesign Home Page - Hero Only

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
-import FeaturedCourses from '../components/home/FeaturedCourses';
-import FeaturedProducts from '../components/home/FeaturedProducts';
-import StudentSuccess from '../components/home/StudentSuccess';
-import LatestNews from '../components/home/LatestNews';
-import NewsletterSubscription from '../components/home/NewsletterSubscription';
 
 const Home = () => {
   return (
     <div className="bg-black">
       <Hero />
-      <FeaturedCourses />
-      <FeaturedProducts />
-      <StudentSuccess />
-      <LatestNews />
-      <NewsletterSubscription />
     </div>
   );
 };
